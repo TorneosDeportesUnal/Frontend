740996369a7915783ff986411003b44af9807b80
Angular CLI <angular-cli@angular.io>
2017-03-22 00:34:22 -0500

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TorneosUnalAppPage } from './app.po';
+
+describe('torneos-unal-app App', () => {
+  let page: TorneosUnalAppPage;
+
+  beforeEach(() => {
+    page = new TorneosUnalAppPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
