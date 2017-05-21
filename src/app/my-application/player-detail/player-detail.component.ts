55bb2912d8b76911da1a59865a22b124b479aa2c
Julian Jimenez <jucjimenezmo@unal.edu.co>
2017-05-21 18:05:02 -0500

    Ajustes en la presentación de la lista de jugadores, se incluyen todos campos, se crean botones editar y eliminar. Se valida el correcto funcionamiento de la actualización de datos de los jugadores.

--- src/app/my-application/player-detail/player-detail.component.ts
+++ src/app/my-application/player-detail/player-detail.component.ts
@@ -14,19 +14,65 @@ export class PlayerDetailComponent implements OnInit {
   @Input('player') player: Player;
   @Output() closeModal: EventEmitter<boolean> = new EventEmitter() ;
 
-  public form = this.fb.group({
-    document: ['', Validators.required],
-    document_type: ['', Validators.required],
-    first_name: ['', Validators.required],
-    last_name: ['', Validators.required],
-    email: ['', Validators.required],
-  });
+  public form: FormGroup;
+  private selectOptionGender: any;
+
 
   constructor(private playerService: PlayerService,
               public fb: FormBuilder,
-              private router: Router) { }
+              private router: Router) {
+
+    this.selectOptionGender = [
+      {
+        value: 'femenino',
+        label: 'Femenino',
+      }, {
+        value: 'masculino',
+        label: 'Masculino',
+      }
+    ];
+
+  }
 
   ngOnInit() {
+
+    console.log('hola documento tipo' + this.player.document_type);
+
+    // creat form model
+    this.form = this.fb.group({
+      document: ['', Validators.required],
+      document_type: ['', Validators.required],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      gender: ['', Validators.required],
+      semester: ['', Validators.required],
+      career: ['', Validators.required],
+      age: ['', Validators.required],
+      contact_phone: ['', Validators.required]
+    });
+
+    // Update form model
+    const oldPlayer = {
+      document: this.player.document,
+      document_type: this.player.document_type,
+      first_name: this.player.first_name,
+      last_name: this.player.last_name,
+      email: this.player.email,
+      gender: this.player.gender,
+      semester: this.player.semester,
+      career: this.player.career,
+      age: this.player.age,
+      contact_phone: this.player.contact_phone
+    };
+
+    (<FormGroup>this.form)
+    .setValue(oldPlayer, { onlySelf: true });
+
+
+    // Update single value
+   // (<FormControl>this.form.controls['first_name'])
+    //  .setValue('John', { onlySelf: true });
   }
 
   closePopUp() {
@@ -44,6 +90,15 @@ export class PlayerDetailComponent implements OnInit {
     const saveElem: any = {
       id_player: this.player['id'],
       first_name: formModel.first_name as string,
+      gender: formModel.gender as string,
+      document: formModel.document,
+      document_type: formModel.document_type,
+      last_name: formModel.last_name,
+      email: formModel.email,
+      semester: formModel.semester,
+      career: formModel.career,
+      age: formModel.age,
+      contact_phone: formModel.contact_phone
     };
 
 
@@ -56,7 +111,7 @@ export class PlayerDetailComponent implements OnInit {
       .subscribe(
         () => {
           this.closePopUp();
-          window.location.reload();
+           window.location.reload();
           console.log('hola entrando a succes de add() del update');
         }
         ,
